4c87392c961b8e4a6caef75fcc289a7f6dec6693
George Rojas <rojas@alcf.anl.gov>
2013-07-25 17:08:53 -0500

    added requirements variable

--- testdir/rundir/startup_mode.py
+++ testdir/rundir/startup_mode.py
@@ -3,7 +3,8 @@ from rttlib import scheduler
 enableCapturing   = True
 enableMaintenance = True
 enableEventCheck  = True
-testName = ''
+test_name         = 'Powerup Mode Test'
+requirements      = '5,6,7,8'
 
 def RunScript(self):
 
