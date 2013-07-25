45594e801e72ed8d12213f5be9bf99d2b6dd7e9f
George Rojas <rojas@alcf.anl.gov>
2013-07-25 10:27:46 -0500

    done with test

--- testdir/startup_mode_test_data.py
+++ testdir/startup_mode_test_data.py
@@ -1,5 +1,5 @@
 
-run_list = [ {'TEST_RUN' : 'r1', 'description': 'Fan1 ok, Fan2 ok at startup mode test'     , 'fan1_fault ': False, 'fan2_fault (enum)': False},
-             {'TEST_RUN' : 'r2', 'description': 'Fan1 Fault Fan2 ok at startup mode'        , 'fan1_fault ': True , 'fan2_fault (enum)': False},
-             {'TEST_RUN' : 'r3', 'description': 'Fan1 ok Fan2 faulted at startup mode'      , 'fan1_fault ': False, 'fan2_fault (enum)': True},
-             {'TEST_RUN' : 'r4', 'description': 'Fan1 faulted Fan2 faulted at startup mode' , 'fan1_fault ': True , 'fan2_fault (enum)': True}]
+run_list = [ {'TEST_RUN' : 'r1', 'description': 'Fan1 ok, Fan2 ok at startup mode test'     , 'fan1_fault (enum)': False, 'fan2_fault (enum)': False},
+             {'TEST_RUN' : 'r2', 'description': 'Fan1 Fault Fan2 ok at startup mode'        , 'fan1_fault (enum)': True , 'fan2_fault (enum)': False},
+             {'TEST_RUN' : 'r3', 'description': 'Fan1 ok Fan2 faulted at startup mode'      , 'fan1_fault (enum)': False, 'fan2_fault (enum)': True},
+             {'TEST_RUN' : 'r4', 'description': 'Fan1 faulted Fan2 faulted at startup mode' , 'fan1_fault (enum)': True , 'fan2_fault (enum)': True}]
