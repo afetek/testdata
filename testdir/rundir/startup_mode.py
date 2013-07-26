be289be926d734177a377c1797126bda2cc32874
George Rojas <rojas@alcf.anl.gov>
2013-07-26 12:11:12 -0500

    uncommented run 5

--- testdir/rundir/startup_mode.py
+++ testdir/rundir/startup_mode.py
@@ -3,7 +3,7 @@ from rttlib import scheduler
 enableCapturing   = True
 enableMaintenance = True
 enableEventCheck  = True
-test_name         = 'Powerup Mode'
+test_name         = 'Startup Mode'
 requirements      = '1,5,6,7,8'
 
 def RunScript(self):
