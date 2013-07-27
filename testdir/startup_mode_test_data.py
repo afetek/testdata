713ffdafb8477e9cbafe3945c2d4bc72cc033934
George Rojas <rojas@alcf.anl.gov>
2013-07-27 11:28:07 -0500

    Fixed run 5. Made powerup to be true. PR 123

--- testdir/startup_mode_test_data.py
+++ testdir/startup_mode_test_data.py
@@ -3,4 +3,4 @@ run_list = [ {'TEST_RUN' : 'r1', 'DESCRIPTION': 'Fans OK at startup'            
              {'TEST_RUN' : 'r2', 'DESCRIPTION': 'Fan1 faulted at startup'         , 'fan1_fault (enum)': True ,  'fan2_fault (enum)': False, 'powerup (enum)': False},
              {'TEST_RUN' : 'r3', 'DESCRIPTION': 'Fan2 faulted at startup'         , 'fan1_fault (enum)': False,  'fan2_fault (enum)': True,  'powerup (enum)': False},
              {'TEST_RUN' : 'r4', 'DESCRIPTION': 'Fan1 and Fan2 faulted at startup', 'fan1_fault (enum)': True ,  'fan2_fault (enum)': True,  'powerup (enum)': False},
-             {'TEST_RUN' : 'r5', 'DESCRIPTION': 'Fans OK, powerup to startup'     , 'fan1_fault (enum)': False , 'fan2_fault (enum)': False, 'powerup (enum)': False}]
+             {'TEST_RUN' : 'r5', 'DESCRIPTION': 'Fans OK, powerup to startup'     , 'fan1_fault (enum)': False , 'fan2_fault (enum)': False, 'powerup (enum)': True}]
